a9796f2feba625d00efc952f2945a29126dc13e5
zachafs <ommaratz@gmail.com>
2023-09-27 10:42:05 -0700

    configuration has been included

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# puppet
+
+package { 'nginx':
+ensure => installed,
+}
+
+file_line { 'install':
+ ensure => 'present',
+ path	=> 'etc/nginx/sites-enabled/default',
+ after	=> 'lesten 80 default_server;',
+ line	=>'rewrite ^/redirect-me https://www.github.com/zachafs permanent;',
+}
+
+file { '/var/www/html/index.html':
+ content => 'hello world!',
+}
+
+service { 'nginx':
+ ensure => running,
+ require => Package ['nginx'],
+}
+ 
