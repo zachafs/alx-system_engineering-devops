a24e1a46eafa893b5f61b875e27aadd2b22959e6
zachafs <ommaratz@gmail.com>
2023-10-23 13:13:51 -0700

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,42 +1,31 @@
 #!/usr/bin/python3
-#better way to export file :
-
-import re
+""" export file  """
 import requests
 import sys
-import csv
 
-REST_API = "https://jsonplaceholder.typicode.com"
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = emp_req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+def main():
+    """ user_id + content """
+    id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    done = f'{todos}&completed=true'
+    notDone = f'{todos}&completed=false'
+    userData = requests.get(f'{url}{users}').json()
+    Name = userData[0].get("name")
+    userName = userData[0].get("username")
+    todosData = requests.get(f'{url}{todos}').json()
+    todosDone = requests.get(f'{url}{done}').json()
+    doneN = len(todosDone)
+    totalN = len(todosData)
+    """Export into csv"""
+    with open(f'{id}.csv', 'w') as f:
+        for todo in todosData:
+            data = f'"{id}","{userName}","{todo.get("completed")}",'
+            data2 = f'"{todo.get("title")}"\n'
+            f.write(data+data2)
 
-            # Define the CSV file name based on the user ID
-            csv_file_name = '{}.csv'.format(id)
 
-            with open(csv_file_name, mode='w', newline='') as csv_file:
-                csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-                # Write the CSV header
-                csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-                # Write each task to the CSV file
-                for task in completed_tasks:
-                    csv_writer.writerow([id, emp_name, str(task.get('completed')), task.get('title')])
-
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            print(f'CSV data has been saved to {csv_file_name}')
-
+if __name__ == "__main__":
+    main()
