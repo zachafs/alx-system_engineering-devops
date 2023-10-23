29f749cbbfaaeff7ea526ac47373d3ccdcbace92
zachafs <ommaratz@gmail.com>
2023-10-23 13:21:25 -0700

    py3

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,31 +1,41 @@
 #!/usr/bin/python3
-""" export file  """
+"""
+This script exports user-specific task data to a CSV file.
+Usage: ./script.py <user_id>
+"""
+
 import requests
 import sys
 
-
 def main():
-    """ user_id + content """
-    id = sys.argv[1]
-    url = f'https://jsonplaceholder.typicode.com/'
-    users = f'users?id={id}'
-    todos = f'todos?userId={id}'
-    done = f'{todos}&completed=true'
-    notDone = f'{todos}&completed=false'
-    userData = requests.get(f'{url}{users}').json()
-    Name = userData[0].get("name")
-    userName = userData[0].get("username")
-    todosData = requests.get(f'{url}{todos}').json()
-    todosDone = requests.get(f'{url}{done}').json()
-    doneN = len(todosDone)
-    totalN = len(todosData)
-    """Export into csv"""
-    with open(f'{id}.csv', 'w') as f:
-        for todo in todosData:
-            data = f'"{id}","{userName}","{todo.get("completed")}",'
-            data2 = f'"{todo.get("title")}"\n'
-            f.write(data+data2)
+    """
+    The main function for retrieving user-specific data and exporting it to a CSV file.
+    """
+    id = sys.argv[1]  # Get the user ID from the command-line argument
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_endpoint = f'/users?id={id}'
+    todos_endpoint = f'/todos?userId={id}'
+    completed_todos_endpoint = f'{todos_endpoint}&completed=true'
 
+    # Retrieve user information
+    user_data = requests.get(f'{base_url}{user_endpoint}').json()
+    name = user_data[0].get("name")
+    username = user_data[0].get("username")
+
+    # Retrieve user's task data
+    todos_data = requests.get(f'{base_url}{todos_endpoint}').json()
+    completed_todos_data = requests.get(f'{base_url}{completed_todos_endpoint}').json()
+    completed_tasks_count = len(completed_todos_data)
+    total_tasks_count = len(todos_data)
+
+    # Export data into a CSV file
+    with open(f'{id}.csv', 'w') as csv_file:
+        for todo in todos_data:
+            csv_data = f'"{id}","{username}","{todo.get("completed")}",'
+            csv_data += f'"{todo.get("title")}"\n'
+            csv_file.write(csv_data)
 
 if __name__ == "__main__":
     main()
+
+
